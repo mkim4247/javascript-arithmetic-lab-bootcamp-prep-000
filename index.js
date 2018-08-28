66b23b223a8354c698a6ce800cdc05041a8f3bee
Michael Kim <m.kim4247@gmail.com>
2018-08-28 20:04:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b
+}
+function subtract (a, b) {
+  return a - b 
+}
+function multiply (a, b) {
+  return a * b
+}
+function divide (a, b) {
+  return a / b
+}
+function inc(n) {
+  return n += 1
+}
+function dec(n) {
+  return n -= 1
+}
+function makeInt(n) {
+  parseInt('n', 10)
+  return n
+}
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+
